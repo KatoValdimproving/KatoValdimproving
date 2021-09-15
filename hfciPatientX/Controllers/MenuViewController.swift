7f739eebf2b1f47704fa4fc36f141de838f41c25
Kato <jose.valdez@improving.com>
2021-09-15 18:06:53 -0500

    Merge and add navigation

--- hfciPatientX/Controllers/MenuViewController.swift
+++ hfciPatientX/Controllers/MenuViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    @IBOutlet weak var menuPresenter: RoundedView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        menuPresenter.addGestureRecognizer(gesture)
         // Do any additional setup after loading the view.
     }
     
 
+    @objc func didTap() {
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "MapViewController") as? MapViewController {
+            present(vc, animated: true, completion: nil)
+        }
+    }
     /*
     // MARK: - Navigation
 
