0acfad1e9af75b637360c684f12fb4eaaf449cce
Angel Coronado Quintero <angel.coronado@improving.com>
2021-12-12 23:59:42 -0600

    Search bar working

--- hfciPatientX/Controllers/Art Walk/GalleryViewController.swift
+++ hfciPatientX/Controllers/Art Walk/GalleryViewController.swift
@@ -13,15 +13,18 @@ import Mappedin
 
 class GalleryViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
     let flowLayout = UICollectionViewFlowLayout()
     var selectedPainting: Painting?
     var allLocations : [MPILocation] = []
     var selectedLocation: MPILocation?
     var mapViewController: MapViewController?
+    var isFiltering = false
+    var filteredPaintings: [Painting] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchBar.delegate = self
         flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
 
         print("width vvv \(self.view.frame.width)")
@@ -104,7 +107,7 @@ class GalleryViewController: UIViewController {
 extension GalleryViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.selectedPainting = paintings[indexPath.row]
+        self.selectedPainting = isFiltering ? filteredPaintings[indexPath.row] :  paintings[indexPath.row]
         
 //        self.selectedLocation = getLocation(name: getBeaconByPaintiingTitle(title: self.selectedPainting!.title)!.location)
         self.performSegue(withIdentifier: "pictureDetail", sender: self)
@@ -119,12 +122,12 @@ extension GalleryViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return paintings.count
+        return isFiltering ? filteredPaintings.count :  paintings.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GalleryCollectionViewCell", for: indexPath) as? GalleryCollectionViewCell
-        let painting = paintings[indexPath.row]
+        let painting = isFiltering ? filteredPaintings[indexPath.row] :  paintings[indexPath.row]
         cell?.setInfo(painting: painting)
         return cell!
     }
@@ -132,4 +135,47 @@ extension GalleryViewController: UICollectionViewDataSource {
     
 }
 
+extension GalleryViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+       
+        filteredPaintings = []
+
+        if searchText == "" {
+            self.isFiltering = false
+            self.collectionView.reloadData()
+            return
+        }
+            
+        for painting in paintings {
+            if painting.title.lowercased().contains(searchText.lowercased()) {
+                filteredPaintings.append(painting)
+            }
+                
+        }
+        
+        self.isFiltering = true
+
+        self.collectionView.reloadData()
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+       // self.isFiltering = true
+     //   self.collectionView.reloadData()
+
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+//        self.isFiltering = false
+//        self.collectionView.reloadData()
+
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+ //       self.isFiltering = false
+//        self.collectionView.reloadData()
+
+    }
+    
+}
 
