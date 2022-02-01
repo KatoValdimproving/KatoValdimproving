0bc59c9f2bf88f7d5bb02b28eec5cad3939002bd
Kato <jose.valdez@improving.com>
2022-02-01 09:12:09 -0600

    Quick fix

--- hfciPatientX/AppDelegate.swift
+++ hfciPatientX/AppDelegate.swift
@@ -414,5 +414,7 @@ extension AppDelegate: MessagingDelegate {
       )
       // TODO: If necessary send token to application server.
       // Note: This callback is fired at each app startup and whenever a new token is generated.
+        
+        //
     }
 }
